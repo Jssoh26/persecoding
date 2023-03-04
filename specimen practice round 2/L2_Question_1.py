75116ce63660d6c796bff698dfa7504e78bb37b0
Jssoh26 <125533828+Jssoh26@users.noreply.github.com>
2023-03-04 16:47:19 +0800

    Update L2_Question_1.py

--- specimen practice round 2/L2_Question_1.py
+++ specimen practice round 2/L2_Question_1.py
@@ -6,8 +6,7 @@ for i in integer:
    if 13 in integer:
     position = integer(13)
     new_list = integer.pop(position)
-    new_new_list = integer.pop(position)
-    total = sum(new_new_list)
+    total = sum(new_list)
     print(total)
 else:
   total = sum(integer)
